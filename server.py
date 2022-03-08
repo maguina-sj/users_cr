4a979ac977cf44e4b91bfbb9f8dc5f0e5ce1de87
maguina-sj <jomaguina90@gmail.com>
2022-03-08 14:43:47 -0800

    assignment crud completed

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request, session
+from flask import Flask, render_template, redirect, request 
 from user import User
 
 app = Flask(__name__)
@@ -8,7 +8,7 @@ def create_user():
     return render_template('newuser.html')
 
 @app.route('/users', methods=["POST"])
-def users():
+def home():
     data = {
         'first_name' : request.form['first_name'],
         'last_name' : request.form['last_name'],
@@ -23,6 +23,44 @@ def show_users():
     print(users)
     return render_template ('index.html', all_users = users)
 
+
+@app.route('/users/<int:id>')
+def view_one(id):
+    data = {
+        'id' : id
+    }
+    user = User.get_one(data)
+    return render_template('oneuser.html', one_user = user )
+
+
+@app.route('/users/<int:id>/edit')
+def edit(id):
+    data = {
+        'id' : id
+    }
+    user = User.get_one(data)
+    return render_template('edituser.html', edit_user = user)
+
+
+@app.route('/users/<int:id>/edit', methods=['POST'])
+def process_edit(id):
+    data = {
+        'id' : id,
+        'first_name' : request.form['first_name'],
+        'last_name' : request.form['last_name'],
+        'email' : request.form['email']
+    }
+    User.update(data)
+    return redirect (f'/users/{id}')
+
+
+@app.route('/users/<int:id>/delete')
+def delete(id):
+    data = {
+        'id' : id
+    }
+    User.delete(data)
+    return redirect('/show_users')
 
 
 if __name__ == "__main__":
